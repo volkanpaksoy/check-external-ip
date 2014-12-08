07ae9e1abc8d6f8c1f9ed478e24981a6173f3079
Volkan Paksoy <volkan.paksoy@gmail.com>
2014-12-08 18:25:02 +0000

    Added x-forwarded-for in a separate field

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    var ip = req.headers['x-forwarded-for'] || 
-     		 req.connection.remoteAddress || 
-     		 req.socket.remoteAddress ||
-     		 req.connection.socket.remoteAddress;
-    res.json({ ipAddress: ip });
+    var forwardedIp = req.headers['x-forwarded-for'];
+    var remoteAddress = req.connection.remoteAddress;
+    res.json({ "ipAddress": remoteAddress, "x-forwarded-for": forwardedIp });
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 18080);
