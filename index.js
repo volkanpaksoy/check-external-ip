b5bf295d0ab27924c851784da5a374fe067d0318
Volkan Paksoy <volkan.paksoy@gmail.com>
2014-12-08 18:05:49 +0000

    Updated IP acquiring method

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    var ip = req.connection.remoteAddress;
+    var ip = req.headers['x-forwarded-for'] || 
+     		 req.connection.remoteAddress || 
+     		 req.socket.remoteAddress ||
+     		 req.connection.socket.remoteAddress;
     res.json({ ipAddress: ip });
 });
 
