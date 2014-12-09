573f24eb98707bea587da231a3defd50f6ad45f8
Volkan Paksoy <volkan.paksoy@gmail.com>
2014-12-09 22:34:58 +0000

    Updated to split proxy chain and get the last entry

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var app = express();
 app.get('/', function (req, res) {
     var remoteAddress = req.headers['x-forwarded-for'] || 
     				  req.connection.remoteAddress;
-    res.json({ "ipAddress": remoteAddress });
+    var ipList = remoteAddress.split(",");
+    var lastProxy = ipList[ipList.length - 1];
+    res.json({ "ipAddress": lastProxy });
 });
 
 app.listen(process.env.PORT || 80);
