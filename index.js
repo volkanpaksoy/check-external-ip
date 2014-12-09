ca17ca8c8f88f9c72c09772bf6ddce6ffb676a8f
Volkan Paksoy <volkan.paksoy@gmail.com>
2014-12-09 22:37:55 +0000

    Temporarily added x-forwarded-for to output to test with proxy chains

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+    var forwarded = req.headers['x-forwarded-for'];
     var remoteAddress = req.headers['x-forwarded-for'] || 
     				  req.connection.remoteAddress;
     var ipList = remoteAddress.split(",");
     var lastProxy = ipList[ipList.length - 1];
-    res.json({ "ipAddress": lastProxy });
+    res.json({ "ipAddress": lastProxy, "x-forwarded-for": forwarded });
 });
 
 app.listen(process.env.PORT || 80);
